e6c5a2a291355d6bbd1c59275b835fb947425d76
Raad <raadsn11@gmail.com>
2023-06-06 15:40:38 +0200

    new exrices1

--- day1/selectors/script.js
+++ day1/selectors/script.js
@@ -4,11 +4,19 @@ const importt = document.getElementsByClassName("important");
 
 
 for (const child of importt) {
-	child.setAttribute("title","Important")
+	child.setAttribute("title","This is an important item")
+	
 }
 
 for (const imgraad of document.images) {
 	if (imgraad.className !== 'important') {
 		imgraad.setAttribute('style','display:none');
+	}
+}
+
+const leng = document.getElementsByTagName('p');
+for (const holder of document.p) {
+	if ( holder == document.querySelectorAll('') {
+
 	}
 }
