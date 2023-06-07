989893c75e84971a746e18b0b519e5222b996851
Raad <raadsn11@gmail.com>
2023-06-07 18:39:38 +0200

    finished the crate task1

--- day2/crate/script.js
+++ day2/crate/script.js
@@ -60,6 +60,19 @@ var myclass = [
     "Athi",
     "Oana"
 ];
+const hexToRGB = (hex) => {
+    const red = parseInt(hex.slice(1, 3), 16);
+    const green = parseInt(hex.slice(3, 5), 16);
+        const blue = parseInt(hex.slice(5, 7), 16);
+
+        return [red, green, blue];
+}
+
+const getTextColor = (hexColor) => {
+    const [red, green, blue] = hexToRGB(hexColor);
+    const brightness = (red * 299 + green * 587 + blue * 114) / 1000;
+        return brightness < 125 ? 'white' : 'black';
+}
 
 let coll = myclass;
 for (var i = 0; i < myclass.length; i++) {
@@ -71,8 +84,10 @@ for (var i = 0; i < myclass.length; i++) {
     people.append(people_p);
     people.append(myclass[i]);
     let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
-    people.setAttribute('style', `background-color: ${randomColor}; order: ${randomNumber}`);
+        let textColor = getTextColor(randomColor);
+    people.setAttribute('style', `background-color: ${randomColor}; color: ${textColor}; order: ${randomNumber}`);
     art.setAttribute('style','display:flex; flex-direction:column');
+
 }
 
 
