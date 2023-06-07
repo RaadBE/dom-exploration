7c646f9a20ffa9466e3193bd224e1d72b34ad3e4
Raad <raadsn11@gmail.com>
2023-06-07 13:59:06 +0200

    finished tasks

--- day1/selectors/script.js
+++ day1/selectors/script.js
@@ -1,11 +1,12 @@
 var element = "This is an important item";
 const importt = document.getElementsByClassName("important");
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 
 
 for (const child of importt) {
 	child.setAttribute("title","This is an important item")
-	
+
 }
 
 for (const imgraad of document.images) {
@@ -14,9 +15,18 @@ for (const imgraad of document.images) {
 	}
 }
 
-const leng = document.getElementsByTagName('p');
-for (const holder of document.p) {
-	if ( holder == document.querySelectorAll('') {
+const span = document.querySelectorAll("p");
+for(const hold of span) {
+  if (hold.className == true) {
+    hold.setAttribute('style', `color: none`);
 
-	}
+  }else if (hold.className == false){
+  let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+  hold.setAttribute('style', `color: ${randomColor}`);
 }
+console.log(hold);
+  console.log('class of pargh is :', hold.className);
+}
+
+
+
